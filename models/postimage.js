df477a6c4f72b4c488a3b5baa74bc2f659234e1a
franiel <66392105+franiiel@users.noreply.github.com>
2025-09-30 15:44:11 -0300

    Agregando dependencias y modelos

---
+++ models/postimage.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PostImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      
+    }
+  }
+  PostImage.init({
+    url: {
+      type: DataTypes.String,
+      unique:true,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'PostImage',
+    timestamps:false
+  });
+  return PostImage;
+};
---
+++ models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Post,
+        {
+          foreignKey: "userId",
+          as: "posts"
+        }
+      )
+    }
+  }
+  User.init({
+    nickname: {
+      type: DataTypes.String,
+      unique:true,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    timestamps:false
+  });
+  return User;
+};
